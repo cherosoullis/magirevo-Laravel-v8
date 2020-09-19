25080e93e896b4ad87aff4c8cb5eb215762b04f2
cherosoullis <cherosoullis@hotmail.com>
2020-09-19 22:41:14 +0300

    Added teams and disabled

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,25 +15,3 @@ mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'pub
     require('postcss-import'),
     require('tailwindcss'),
 ]);
-
-  mix.styles([
-  'node_modules/bootstrap-slider/dist/css/bootstrap-slider.css',
-
-  //'node_modules/select2/dist/css/select2.min.css'
-],  'public/css/bootstrap-slider.css');
-
-mix.styles([
-'resources/css/foodstaff.css'
-],  'public/css/foodstaff.css');
-
-
-mix.sass('resources/sass/application.scss', 'public/css');
-
-    mix.scripts([
-         // 'resources/js/slider.js',
-         'resources/js/select.js'
-      ],  'public/js/compine.js');
-
-      mix.scripts([
-              'node_modules/slugify/slugify.js',
-          ],  'public/js/slug.js');
